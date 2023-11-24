1c2c4aeb87fdc8de24a392e036d27acc82a641a5
lqh <208478579@qq.com>
2023-11-25 00:50:35 +0800

    冒泡排序

--- C语言排序算法/冒泡排序.c
+++ C语言排序算法/冒泡排序.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<stdbool.h>
-int* Bubble_sort(int* p,int n);
-int main(){
-//冒泡排序算法，给定整数数组
-	
-	int n,i;
-	scanf("%d",&n);
-	int* a =(int*)malloc(n*sizeof(int));
-	for(i =0; i<n; i++){
-		scanf("%d",a+i);
-	}
-	int *p = Bubble_sort(a,n);
-	for(i=0;i<n;i++){
-		printf("%d ",*(p+i));
-	} 
-	free(a);
-	return 0;
-}
-int* Bubble_sort(int* a,int n){
-	bool flag = false;//标记变量，如果数组本身有序，则flag始终为负
-	int tmp;
+void Bubble_sort(int a[],int n);
+void Bubble_sort(int a[],int n){
+	int flag = 0;
+	int temp = 0;
 	int i,j;
-	int * p=a;
-	for( i=0;i<n-1;i++){
-		for(j=1;j<n-i;j++){  //对某一趟i，进行n-i-1次比较
+	for(i=1; i<n; i++){
+		for(j=n-1; j>i-1;j--){
 			if(a[j-1]>a[j]){
-				tmp = a[j];
-				a[j]= a[j-1];
-				a[j-1]=tmp;
-				flag = true;
+				temp = a[j-1];
+				a[j-1]=a[j];
+				a[j]= temp;
+				flag = 1;
 			}
 		}
-		while(flag ==false ){
-			return a ;
+		while(flag ==0){
+			return;
 		}
+	}
+	return;
+}//
+int main(){
+	int n,i ;
+	scanf("%d",&n);
+	int* a = (int*)malloc(sizeof(int)*n);//动态分配n*sizeof(int)大小的内存 
+	for(i=0; i<n;i++){
+		scanf("%d",a+i);
+	}
+	Bubble_sort(a,n);
+	for(i=0; i<n; i++){
+		printf("%d ",a[i]);
 	} 
-	return p;
+	free(a);
+	return  0;
 }
